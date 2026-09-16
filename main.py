69c73c22e02df984a83a7234a15ccc55b4502feb
dxrxvk <130661539+dxrxvk@users.noreply.github.com>
2026-09-16 15:27:05 -0300

    environement setup

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 def main():
     print("Hello from langchain-course!")
+    print(os.environ.get("OLLAMA_API_KEY"))
 
 
 if __name__ == "__main__":
